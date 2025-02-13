dd9c28ae02622315c540fae4ce4d8734aacf6833
hadesds <hadessds@gmail.com>
2025-02-13 09:42:38 -0300

    Atividade Conversao de Tipos

--- src/fundamentos/ConversoesTipos.java
+++ src/fundamentos/ConversoesTipos.java
@@ -1,4 +1,20 @@
 package fundamentos;
 
+//Atividade referente a Conversões de tipos
+
 public class ConversoesTipos {
+    public static void main(String[] args) {
+
+        //double para int
+        double primitivoX = 12.0;
+        int castingX = (int) primitivoX;
+        System.out.println("Valor primitivo double: " + primitivoX);
+        System.out.println("Valor wrapper int: " + castingX);
+
+        //String para Int
+        String numeroS = "20";
+        int conversao = Integer.parseInt(numeroS);
+        System.out.println("Valor String: " + numeroS);
+        System.out.println("Valor int: " + conversao);
+    }
 }
