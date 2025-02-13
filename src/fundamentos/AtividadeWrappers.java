b8d2185c4c2aa3d0c59bc55342f624b78e2bfd3f
hadesds <hadessds@gmail.com>
2025-02-13 09:13:31 -0300

    Atividade Wrappers

---
+++ src/fundamentos/AtividadeWrappers.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+//Atividade relacionada a Wrappers
+
+public class AtividadeWrappers {
+    public static void main(String[] args) {
+
+        //Conversão explícita de integer
+        Integer a = 19;
+        System.out.println(a.intValue());
+
+        //Conversão de double para Double
+        double b = (Double) 123.0;
+        System.out.println(b);
+    }
+}
