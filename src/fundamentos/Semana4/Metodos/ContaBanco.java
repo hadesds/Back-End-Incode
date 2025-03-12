735a1f832f5ea53da363da38da423e4d08153745
hadesds <hadessds@gmail.com>
2025-03-12 11:54:37 -0300

    Conta banco

---
+++ src/fundamentos/Semana4/Metodos/ContaBanco.java
@@ -0,0 +1,31 @@
+package fundamentos.Semana4.Metodos;
+
+public class ContaBanco {
+    //Atributos
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+
+    //Metodos
+    public void abrirConta(){
+
+    }
+    public void fecharConta() {
+
+    }
+    public void depositar() {
+
+    }
+    public void sacar() {
+
+    }
+    public void pagarMensal() {
+
+    }
+    //Metodos Especiais
+    public void ContaBanco() {
+
+    }
+}
