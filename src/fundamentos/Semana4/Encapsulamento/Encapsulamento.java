8ecace469e6b63b99a0275c40987ad9753a08596
hadesds <hadessds@gmail.com>
2025-03-14 11:55:02 -0300

    Interface com Encapsulamento

---
+++ src/fundamentos/Semana4/Encapsulamento/Encapsulamento.java
@@ -0,0 +1,11 @@
+package fundamentos.Semana4.Encapsulamento;
+
+public class Encapsulamento {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.play();
+        c.maisVolume();
+        c.abrirMenu();
+    }
+}
