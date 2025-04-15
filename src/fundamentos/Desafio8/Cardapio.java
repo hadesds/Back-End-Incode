4f9da71819c73536bbcbe210b60c75c1bcf63939
hadesds <hadessds@gmail.com>
2025-04-15 09:09:44 -0300

    Desafio semana 8

--- src/fundamentos/Desafio8/Cardapio.java
+++ src/fundamentos/Desafio8/Cardapio.java
@@ -7,7 +7,8 @@ public class Cardapio {
     public static void main(String[] args) {
         Refeicao semana = new Refeicao();
         semana.montarRefeicao();
-        semana.mostrarMenu();    }
+        semana.mostrarMenu();
+    }
 }
 
 
--- src/fundamentos/Desafio8/Refeicao.java
+++ src/fundamentos/Desafio8/Refeicao.java
@@ -12,42 +12,51 @@ public class Refeicao extends Alimentos {
         Segunda, Terça, Quarta, Quinta, Sexta
     }
 
+    enum turnoDaSemana {
+        Matutino, Noturno
+    }
+
     ArrayList<String> cardapioSemanal = new ArrayList<>();
     Scanner entrada = new Scanner(System.in);
 
     public void montarRefeicao() {
         for (int dia = 0; dia < 5; dia++) {
-            System.out.println("--- Dia " + diasDaSemana.values()[dia] + "---");
+            for (int turno = 0; turno < 2; turno++) {
+                System.out.println("--- Dia: " + diasDaSemana.values()[dia] + "---");
+                System.out.println("--- Turno: " + turnoDaSemana.values()[turno] + "---");
 
-            System.out.println("Escolha sua salada: ");
-            saladas.forEach((k, v) -> System.out.println(k + " - " + v));
-            int escolhaSalada = entrada.nextInt();
-            String saladaEscolhida = saladas.get(escolhaSalada);
+                saladas.forEach((k, v) -> System.out.println(k + " - " + v));
+                System.out.println("Escolha sua salada: ");
+                int escolhaSalada = entrada.nextInt();
+                String saladaEscolhida = saladas.get(escolhaSalada);
 
-            System.out.println("Escolha sua Prato Principal: ");
-            pratos.forEach((k, v) -> System.out.println(k + " - " + v));
-            int escolhaPrato = entrada.nextInt();
-            String pratoEscolhido = pratos.get(escolhaPrato);
+                pratos.forEach((k, v) -> System.out.println(k + " - " + v));
+                System.out.println("Escolha sua Prato Principal: ");
+                int escolhaPrato = entrada.nextInt();
+                String pratoEscolhido = pratos.get(escolhaPrato);
 
-            System.out.println("Escolha sua Acompanhamentos: ");
-            acompanhamentos.forEach((k, v) -> System.out.println(k + " - " + v));
-            int escolhaAcompanhamento = entrada.nextInt();
-            String acompanhamentoEscolhido = acompanhamentos.get(escolhaSalada);
+                acompanhamentos.forEach((k, v) -> System.out.println(k + " - " + v));
+                System.out.println("Escolha sua Acompanhamentos: ");
+                Integer escolhaAcompanhamento = entrada.nextInt();
+                String acompanhamentoEscolhido = acompanhamentos.get(escolhaSalada);
 
-            String refeicao = diasDaSemana.values()[dia] + " | Salada: " + saladaEscolhida +
-                    "| Prato: " + pratoEscolhido +
-                    "| Acompanhamento: " + acompanhamentoEscolhido;
-            cardapioSemanal.add(refeicao);
-            System.out.println("\nRefeição adicionada com sucesso!\n");
+                String refeicao = diasDaSemana.values()[dia] + "[" + turnoDaSemana.values()[turno] + "]" +" | Salada: " + saladaEscolhida +
+                        "| Prato: " + pratoEscolhido +
+                        "| Acompanhamento: " + acompanhamentoEscolhido;
+                cardapioSemanal.add(refeicao);
+                System.out.println("\nRefeição adicionada com sucesso!\n");
+            }
         }
     }
+
     public void mostrarMenu() {
         System.out.println("------ CARDAPIO SEMANAL -----");
-        for(String dia:cardapioSemanal){
+        for (String dia : cardapioSemanal) {
+            System.out.println("-----------------------");
             System.out.println(dia);
         }
-    }
 
+    }
 }
 
 
